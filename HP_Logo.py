2e044d95e846e84239f524abb470a651d31a7be6
Vinit314 <154413350+Vinit314@users.noreply.github.com>
2024-12-31 10:16:51 +0530

    Update Harry Potter Deadly Hollows Logo.py

--- HP_Logo.py
+++ HP_Logo.py
@@ -1,34 +1,48 @@
-# This code we will make logo of Deadly Hollows...
-# from Harry Potter Franchise using Turtle module.
-# The logo consist of a straight line, a circle and a triangle
-
-# all the number shown are taken by trail and error method
+# This code draws a logo of Deadly Hollows
+# from Harry Potter Franchise
 import turtle
+import math
 
-t = turtle.Turtle()
+# Below given x is the side of the equilateral triangle.
+# All other dimensions are calculated by this parameter.
+x = 400  # This parameter is changeable
 
-# Next four line is to get the position of cursor
-# at the point where all three geometrical figures meet
-t.penup()
-t.right(90)
-t.backward(200)
-t.pendown()
+dr = turtle.Turtle()
+y = x/2
+# z is denoted for Apex height
+# z = x * math.sin(math.degrees(60))
+z = (math.sqrt(3) * x) / 2
 
-# this will make straight line
-t.forward(400)
+# This is to get to the starting position
+dr.right(90)
+dr.penup()
+dr.backward(100)
+dr.pendown()
 
-# this will make the circle
-t.left(90)
-t.circle(133.2)
+# This is to draw a straight line
+dr.forward(z)
+dr.right(90)
 
-# this will make the triangle
-t.forward(231)
+# This is draw the equilateral triangle
+dr.forward(y)
+dr.right(120)
+dr.forward(x)  # you can use loop here
+dr.right(120)
+dr.forward(x)
+dr.right(120)
+dr.forward(y)
 
-for i in range(2):
-    t.left(120)
-    t.forward(462)
+# The turtle drawing circle turning left
+# to get the position we turned it to 180 degrees
+dr.right(180)
 
-t.left(120)
-t.forward(231)
+# this is get the dimensions for the circle
+# r is denoted for radius
+# r = y/math.tan(math.degrees(60))
+r = (x * math.sqrt(3)) / 6
+
+# This is draw the circle
+dr.circle(r)
 
 turtle.done()
+# Thank you
